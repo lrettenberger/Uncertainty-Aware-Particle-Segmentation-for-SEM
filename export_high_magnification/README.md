b193c62730e53e418c698567d9d56c548c11676d
Luca <Luca.Rettenberger@kit.edu>
2023-11-16 17:32:36 +0100

    update

--- export_high_magnification/README.md
+++ export_high_magnification/README.md
@@ -8,5 +8,8 @@ To create the conda environment.
 - You can also provide a single file: `python run_model.py --input /some/other/dir/image.png`
 - Only `.tiff`, `.tif`, and `.png` inputs are supported.
 - Images should be provided as `uint8` files in the range `[0,255]`. 
-- An overlay image of the predicted particles and a `.txt` file with estimated parameters will be created for each image.
 - With the default installation this will run on the CPU, so it will probably be rather slow. If you have a GPU, you can install onnxruntime-gpu via pip to have better performance!
+- This implementation expects images to be in the size `(1920,1200)`. Any other size is resized to be `(1920,1200)`. If the image size is not a multiple of `(1920,1200)` the results will most likely be bad. 
+- The script will create a `out/` directory in which the segmentation masks are stored
+- One file with the original file name and the suffix `0_uint16_instance_mask_confident.tiff` will be generated that contains the particles found with high confidence, one file with the suffix `0_uint16_instance_mask_not_confident.tiff` containing the particles with low confidence, and one file with the suffix `0_uint16_instance_mask_combined.tiff` with all found particles (no distinction).
+- The detected particles are uint16 integer encoded.
--- export_low_magnification/run_model.py
+++ export_low_magnification/run_model.py
@@ -29,8 +29,7 @@ parser.add_argument(
     '--input',
     dest='input',
     type=str,
-    # CHANGE BACK TO RELATIVE PATH
-    default='/home/ws/kg2371/projects/Machine-Learning-Particle-Morphology-Estimation/export_low_magnification/images',
+    default='./images',
     help='If the path is an image, the image will be processed. If its a path, the whole directory will be processed.'
 )
 args = parser.parse_args()
@@ -53,47 +52,8 @@ for input_image_path in pbar:
     # Load image
     image = load_image_from_path(input_image_path)
 
-    # Get segmentation mask (distance map)
+    # Get segmentation mask
     mask_not_confident, mask_confident, mask_combined = get_segmentation_mask(image,ort_session)
-
-    # outputs for not confident
-    # Visualize the results and export as an image
-    overlay = visualize_instances_map(cv2.cvtColor(image, cv2.COLOR_GRAY2RGB), mask_not_confident, line_thickness=3)
-    # Resize all images to input image dimensions for writing
-    #cv2.imwrite(f'{output_path}/{image_name.replace(".png","")}_overlay_not_confident.png',overlay)
-    cv2.imwrite(f'{output_path}/{image_name.replace(".png","")}_uint16_instance_mask_not_confident.png',mask_not_confident.astype(np.int16)*(255/np.max(mask_not_confident)))
-    #tifffile.imwrite(f'{output_path}/{image_name.replace(".png","")}_uint16_instance_mask_not_confident.tiff',mask_not_confident.astype(np.int16))
-    # particle areas
-    f = open(f'{output_path}/{image_name.replace(".png","")}_particles_sizes.txt', "a")
-    f.write("Confidence,Index,Size_Pixels,Size_Microns\n")
-    for i in np.unique(mask_not_confident):
-        if i==0:
-            continue
-        area_pixels = (mask_not_confident==i).sum()
-        # we assume a size of 80x80 microns to 1024x1024 px
-        area_microns = area_pixels * (80/1024)
-        f.write(f"Unsure,{int(i)},{area_pixels:.2f},{area_microns:.2f}\n")
-
-    # outputs for confident
-    # Visualize the results and export as an image
-    overlay = visualize_instances_map(cv2.cvtColor(image, cv2.COLOR_GRAY2RGB), mask_confident, line_thickness=3)
-    # Resize all images to input image dimensions for writing
-    #cv2.imwrite(f'{output_path}/{image_name.replace(".png","")}_overlay_confident.png',overlay)
-    #tifffile.imwrite(f'{output_path}/{image_name.replace(".png","")}_uint16_instance_mask_confident.tiff',mask_confident.astype(np.int16))
-    cv2.imwrite(f'{output_path}/{image_name.replace(".png","")}_uint16_instance_mask_confident.png',mask_confident.astype(np.int16)*(255/np.max(mask_confident)))
-    for i in np.unique(mask_confident):
-        if i==0:
-            continue
-        area_pixels = (mask_confident==i).sum()
-        # we assume a size of 80x80 microns to 1024x1024 px
-        area_microns = area_pixels * (80/1024)
-        f.write(f"Sure,{int(i)},{area_pixels:.2f},{area_microns:.2f}\n")
-
-
-    # outputs for combined
-    # Visualize the results and export as an image
-    overlay = visualize_instances_map(cv2.cvtColor(image, cv2.COLOR_GRAY2RGB), mask_combined, line_thickness=3)
-    # Resize all images to input image dimensions for writing
-    cv2.imwrite(f'{output_path}/{image_name.replace(".png","")}_overlay_combined.png',overlay)
+    tifffile.imwrite(f'{output_path}/{image_name.replace(".png","")}_uint16_instance_mask_not_confident.tiff',mask_not_confident.astype(np.int16))
+    tifffile.imwrite(f'{output_path}/{image_name.replace(".png","")}_uint16_instance_mask_confident.tiff',mask_confident.astype(np.int16))
     tifffile.imwrite(f'{output_path}/{image_name.replace(".png","")}_uint16_instance_mask_combined.tiff',mask_combined.astype(np.int16))
-    f.close()
